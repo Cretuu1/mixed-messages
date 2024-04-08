afa18e5dd70e06bf171f3692a7c850ff8f4b0e67
Cretu Andrei <andycrta@gmail.com>
2024-04-09 02:49:56 +0300

    Gemini, Cancer, Leo

--- script.js
+++ script.js
@@ -2,20 +2,30 @@ const signs = ['Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', '
 const randomP = ['You are having good luck.', 'You are having bad luck.', 'You should stay inside.', 'A bad thing will happen to you.', 'A good thing will happen to you.', 'Everyone will treat you nice.', 'Everyone will treat you bad.', 'Everyone will be making jokes about you.', 'Everyone will take your side in an argument.', 'You will get a gift.', 'You will have a good day.', 'You will have a bad day.'];
 
 const horoscponeGen = (month, day) => {
-    if(day > 31){
-        return 'Invalid date';
-    };
+
     if (day <= 31 && day > 0){
         let pLength = randomP.length - 1;
         let randIndex = Math.floor(Math.random() * pLength);
         if(month === 'March' && day >= 21 || month === 'April' && day <= 19) {
             return `Your sign is: ${signs[signs.indexOf('Aries')]}`;
-        }
+        };
         if(month === 'April' && day >= 20 || month === 'May' && day <= 20){
             return `Your sign is: ${signs[signs.indexOf('Taurus')]}`;
-        }        
-}}
+        }; 
+        if(month === 'May' && day >= 21 || month === 'June' && day <= 21){
+            return `Your sign is: ${signs[signs.indexOf('Gemini')]}`;
+        };
+        if(month === 'June' && day >= 22 || month === 'July' && day <= 22){
+            return `Your sign is: ${signs[signs.indexOf('Cancer')]}`;
+        };
+        if(month === 'July' && day >= 23 || month === 'August' && day <= 22){
+            return `Your sign is: ${signs[signs.indexOf('Leo')]}`;
+        }         
+} else {
+    return 'Invalid date.'
+}
+}
 
-console.log(horoscponeGen('April', 24))
+console.log(horoscponeGen('August', 22))
 
 //return `${randomP[randIndex]}`
